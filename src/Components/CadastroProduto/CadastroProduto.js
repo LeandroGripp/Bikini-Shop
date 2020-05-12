4e05b560a221ab6bea49453f4416574860684d5d
Leandro Gripp <lggcontas@gmail.com>
2020-05-12 09:34:44 -0300

    Alteração de nomes para ficar mais compreensível e criação do módulo "Produto"

--- src/Components/CadastroProduto/CadastroProduto.js
+++ src/Components/CadastroProduto/CadastroProduto.js
@@ -1,5 +1,5 @@
 import React, {useState} from "react";
-import "./style.css";
+import "./CadastroProduto.css";
 
 
 
--- src/Components/CadastroUsuario/CadastroUsuario.js
+++ src/Components/CadastroUsuario/CadastroUsuario.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import './CadastroUsuario.css';
 
 export function CadastroUsuario (props) {
   return (
