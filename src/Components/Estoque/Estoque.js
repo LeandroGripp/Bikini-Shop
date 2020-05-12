5a71bc31a34989bbbccd33b5a295e3389e412297
Leandro Gripp <lggcontas@gmail.com>
2020-05-12 09:37:49 -0300

    Alteração de nomes para ficar mais compreensível e criação do módulo "Produto" (no branch lateral)

--- src/Components/Estoque/Estoque.js
+++ src/Components/Estoque/Estoque.js
@@ -1,9 +1,12 @@
 import React from 'react';
+import './Estoque.css';
+import { Produto } from '../Produto/Produto';
 
 export function Estoque (props) {
   return (
     <div>
       Estoque
+      {/* <Produto produto={props.produtos[0]}/> */}
     </div>
   )
 }
