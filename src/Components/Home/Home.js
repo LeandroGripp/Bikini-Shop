1540eeae5239f953f5ca1070761a38b6b17be660
Daniel <danielbenica7@gmail.com>
2020-05-18 16:06:02 -0300

    Homepage

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -1,9 +1,20 @@
 import React from "react";
+import "./Home.css";
+import { Link, Redirect } from "react-router-dom";
 
 export function Home(props) {
+  function handleLogin(){
+    return console.log("testando");
+  }
   return (
-    <div>
-      <img src="https://i.pinimg.com/originals/76/5d/f2/765df239c4b33a4cfc1128cf6cd3b4e2.png" alt="Monica na praia" />
+    <div className="background d-flex ">
+      <div className="d-flex flex-column">
+        <div className="imgcontainer d-flex flex-column">
+          <img className="logo" alt="logo" src="/homelogo.jpg" />
+        </div>
+          <button onClick={handleLogin}className="logar mx-auto" >Entrar</button>
+          <button className="cadastrar mx-auto">Cadastrar</button>
+      </div>
     </div>
   );
 }
