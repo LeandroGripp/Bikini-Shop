b3bf49b6e70b9e29fb41685f6540536b337d13c9
Daniel <danielbenica7@gmail.com>
2020-05-18 16:20:25 -0300

    Conserto de bug gitkraken

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -1,19 +1,21 @@
 import React from "react";
 import "./Home.css";
-import { Link, Redirect } from "react-router-dom";
+import { Link,  } from "react-router-dom";
 
 export function Home(props) {
-  function handleLogin(){
-    return console.log("testando");
-  }
+
   return (
     <div className="background d-flex ">
       <div className="d-flex flex-column">
         <div className="imgcontainer d-flex flex-column">
           <img className="logo" alt="logo" src="/homelogo.jpg" />
         </div>
-          <button onClick={handleLogin}className="logar mx-auto" >Entrar</button>
-          <button className="cadastrar mx-auto">Cadastrar</button>
+          <Link to="/login">
+            <button className="logar mx-auto" >Entrar</button>
+          </Link>
+          <Link to="/CadastroUsuario">
+            <button className="cadastrar mx-auto">Cadastrar</button>
+          </Link>
       </div>
     </div>
   );
