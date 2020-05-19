05ad3737ecd1d3cb71cef556b4bfa7f15ed24de4
Leandro Gripp <lggcontas@gmail.com>
2020-05-19 12:01:24 -0300

    Adição de ícones para os produtos

--- src/Components/Produto/Produto.js
+++ src/Components/Produto/Produto.js
@@ -1,16 +1,26 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Produto.css";
 
 export function Produto(props) {
-  return (
-    <div className="row product-header product-container py-3 px-0 my-1 ">
-      <div className="col-1 px-0">{props.produto.ref}</div>
-      <div className="col-5 px-0">{props.produto.name}</div>
-      <div className="col-2 px-0">{props.produto.type}</div>
-      <div className="col-1 px-0">{props.produto.size}</div>
-      <div className="col-2 px-0">{props.produto.price}</div>
-      <div className="col-1 px-0">{props.produto.units}</div>
-    </div>
-  );
+  const [editing, setEditing] = useState(false);
+  if (editing) {
+    return (
+      <div className="row product-header product-container py-3 px-0 my-1 "></div>
+    );
+  } else {
+    return (
+      <div className="d-flex align-items-center justify-content-between">
+        <div className="row product-header product-container py-3 px-0 my-1 ">
+          <div className="col-1 px-0">{props.produto.ref}</div>
+          <div className="col-5 px-0">{props.produto.name}</div>
+          <div className="col-2 px-0">{props.produto.type}</div>
+          <div className="col-1 px-0">{props.produto.size}</div>
+          <div className="col-2 px-0">{props.produto.price}</div>
+          <div className="col-1 px-0">{props.produto.units}</div>
+        </div>
+        <span className="material-icons">edit</span>
+        <span class="material-icons">delete</span>
+      </div>
+    );
+  }
 }
-
